0ac765c3916e07502f5e2cd59724cce9e3059c7f
Elixir Magic <123097845+Elixirmagics@users.noreply.github.com>
2023-02-25 01:29:21 +0500

    Update Bitcoin.js

--- models/Bitcoin.js
+++ models/Bitcoin.js
@@ -3,25 +3,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bitcoinSchema = new Schema({
-  
   deposit: { 
     type: String, 
     required: true 
   },
-  
+
   amount: { 
     type: Number,
     require: true,
   },
-  
-  withdraw: {
+
+  withdraw: { 
     type: String,
     require: true,
   },
-  
-  network: {
+
+  network: { 
     type: String,
-    required: true,
   },
 
   address: { 
